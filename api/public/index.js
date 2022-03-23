ffadf5eab0fdd0d56425d007568ff6af291c2b28
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-03-24 00:50:08 +0100

    fix: date display

--- api/public/index.js
+++ api/public/index.js
@@ -2,16 +2,16 @@
 function timeSince(date) {
   const seconds = Math.floor((new Date() - date) / 1000);
   let interval = seconds / 31536000;
-  if (interval > 1) return Math.floor(interval) + " years";
+  if (interval > 1) return Math.floor(interval) + " year(s)";
   interval = seconds / 2592000;
-  if (interval > 1) return Math.floor(interval) + " months";
+  if (interval > 1) return Math.floor(interval) + " month(s)";
   interval = seconds / 86400;
-  if (interval > 1) return Math.floor(interval) + " days";
+  if (interval > 1) return Math.floor(interval) + " day(s)";
   interval = seconds / 3600;
-  if (interval > 1) return Math.floor(interval) + " hours";
+  if (interval > 1) return Math.floor(interval) + " hour(s)";
   interval = seconds / 60;
-  if (interval > 1) return Math.floor(interval) + " minutes";
-  return Math.floor(seconds) + " seconds";
+  if (interval > 1) return Math.floor(interval) + " minute(s)";
+  return Math.floor(seconds) + " second(s)";
 }
 
 (async () => {
