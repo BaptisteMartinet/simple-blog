586caaec850c31526521118e83243efffd17378a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-03-25 00:31:08 +0100

    feat: add register page

---
+++ public/scripts/register.js
@@ -0,0 +1,19 @@
+
+async function register(args)
+{
+  if (!args.fullName || !args.email || !args.password)
+    return console.error('Args must be filled')
+  await fetch('/register', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(args),
+  });
+}
+
+document.querySelector("#btn-submit").addEventListener("click", async (event) => {
+  event.preventDefault();
+  const fullName = document.getElementById('fullName').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  register({ fullName, email, password });
+}, false);
