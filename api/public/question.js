bcc5bfe9e7b644e3da111a2eb53cda1dce589113
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-03-24 01:42:51 +0100

    feat: enhance question page

--- api/public/question.js
+++ api/public/question.js
@@ -4,8 +4,11 @@
   if (!urlParams.has('id'))
     return console.error('An id needs to be provided in the url.');
   const postId = urlParams.get('id');
+  const post = await (await fetch(`/post?id=${postId}`)).json();
+  const user = await (await fetch(`/user?id=${post.userId}`)).json();
 
-  const res = await fetch(`/post?id=${postId}`);
-  const post = await res.json();
-  console.log(post);
+  const questionTitle = document.getElementById('question-title');
+  questionTitle.textContent = post.title;
+  const questionMetadata = document.getElementById('question-metadata');
+  questionMetadata.textContent = `Asked on ${new Date(post.created_at).toUTCString()} by ${user.fullName}. Viewed ${post.views} times`;
 })();
