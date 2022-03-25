19877950cc4bfa24ada0366ae8a5879e5ecc799a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-03-25 22:05:20 +0100

    feat: change jwt cookie expiration date

--- api/router.js
+++ api/router.js
@@ -142,7 +142,7 @@ router.post('/login', (req, res) => {
   if (!bcrypt.compareSync(password, user.password))
     return res.status(400).send(`Invalid credentials`);
   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: "2h" });
-  res.cookie('x-access-token', token, { expires: new Date(Date.now() + 20000 /* 86400000 */), httpOnly: true });
+  res.cookie('x-access-token', token, { expires: new Date(Date.now() + 86400000), httpOnly: true });
   return res.status(200).json(user);
 });
 
