d7f0e6acc65cfa0bb405c0c4f872d0c6acbc7ebb
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-03-26 21:58:37 +0100

    feat: add comment section on question page

--- public/scripts/question.js
+++ public/scripts/question.js
@@ -1,9 +1,33 @@
+async function generateComments(postId)
+{
+  const commentsRes = await fetch(`/api/comments?postId=${postId}`);
+  if (!commentsRes.ok)
+    return;
+  const comments = await commentsRes.json();
+  const commentList = document.querySelector('.comments-container');
+  for (const comment of comments) {
+    const commentTemplate = `
+      <li class="comment-container">
+        <div class="comment-body">
+          <p>${comment.body}</p>
+        </div>
+        <div class="comment-user">
+          <p>Answered yesterday by Baptiste Martinet</p>
+        </div>
+      </li>
+    `;
+    commentList.innerHTML += commentTemplate;
+  }
+}
+
 (async () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   if (!urlParams.has('id'))
     return console.error('An id needs to be provided in the url.');
   const postId = urlParams.get('id');
+
+  generateComments(postId); //Comments will be loaded asynchronously
 
   const postRes = await fetch(`/api/post?id=${postId}`);
   if (!postRes.ok)
