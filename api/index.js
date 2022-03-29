32517f96f112f9a1a53f64d45b5b516b5df27312
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-03-29 16:21:36 +0200

    feat: implement mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 const routes = require('./router');
 
 /*
@@ -9,12 +10,15 @@ const routes = require('./router');
   and start the server.
 */
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api', routes);
-app.use(express.static('public', { extensions: [ 'html' ] }));
-
-app.listen(PORT, () => { console.info(`Server running on port: ${PORT}`); });
+(async () => {
+  await mongoose.connect(process.env.DATABASE_NAME);
+  console.info('Database successfully connected.');
+  const app = express();
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use('/api', routes);
+  app.use(express.static('public', { extensions: ['html'] }));
+  app.listen(PORT, () => { console.info(`Server running on port: ${PORT}`); });
+})();
